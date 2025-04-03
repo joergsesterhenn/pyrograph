aab83897d2aa8bebe429ea7d6a1f770f8f8ed3e8
Jörg Sesterhenn <joerg.sesterhenn@gmail.com>
2025-04-03 21:50:09 +0200

    everything in place

--- pyrograph/main.py
+++ pyrograph/main.py
@@ -6,19 +6,20 @@ from pygame.event import Event
 
 def main():
     screen: Surface = draw_screen()
+    rotor: Rotor = Rotor(screen, radius=75, center_x=0, center_y=0)
     running: bool = True
     while running:
         for event in pygame.event.get():
-            running = check_for_quit(event)
-            draw_stator(screen)
-            pygame.display.flip()
-
+            check_for_quit(event)
+        draw_stator(screen)
+        rotor.rotate()
+        pygame.display.flip()
     pygame.quit()
 
 
 def check_for_quit(event: Event):
     if event.type == pygame.QUIT:
-        return False
+        pygame.quit()
 
 
 def draw_screen(
@@ -44,6 +45,46 @@ def draw_stator(
         radius=radius,
         width=line_width,
     )
+
+
+class Rotor:
+    def __init__(
+        self,
+        surface: Surface,
+        radius: int,
+        center_x: int,
+        center_y: int,
+        line_color=Color("red"),
+        rotor_color=Color("black"),
+    ):
+        self.surface = surface
+        self.radius = radius
+        self.center_x = center_x
+        self.center_y = center_y
+        self.rotor_color = rotor_color
+        self.line_color = line_color
+        self.line: list[int:int] = []
+
+    def center(self) -> tuple[int, int]:
+        return (self.center_x, self.center_y)
+
+    def rotate(self):
+        # move rotor one step along stator
+        self.center_x += 1
+        self.center_y += 1
+        self.line.append(self.center())
+        # draw rotor
+        self.draw_rotor()
+        self.draw_line()
+
+    def draw_rotor(self):
+        pygame.draw.circle(
+            self.surface, self.rotor_color, self.center(), self.radius, 2
+        )
+
+    def draw_line(self):
+        if self.line and len(self.line) > 1:
+            pygame.draw.lines(self.surface, self.line_color, False, self.line, 2)
 
 
 if __name__ == "__main__":
